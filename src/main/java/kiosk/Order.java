6069defab771123fdd5831274ce06a4bb6ed6705
Goran <cvijanovicgoran78@gmail.com>
2026-01-23 18:53:57 +0100

    Fix second insert error

--- src/main/java/kiosk/Order.java
+++ src/main/java/kiosk/Order.java
@@ -2,6 +2,8 @@ package kiosk;
 
 import lombok.Data;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
@@ -27,6 +29,7 @@ public class Order implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     private Instant placedAt;
--- src/main/java/kiosk/Taco.java
+++ src/main/java/kiosk/Taco.java
@@ -24,7 +24,7 @@ public class Taco {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
 
     private Instant createdAt;
 
